1e9ec5cac8fa4eb051f04267478c05f6a4f7bdd2
MartyVos <marty.vos@student.hu.nl>
2018-12-21 14:07:00 +0100

    Fixed getPrime

--- 3-getPrime.py
+++ 3-getPrime.py
@@ -1,23 +1,3 @@
-# generateList()
-# This function generates a list of values in a specified range and returns the generated list.
-# Parameters:
-#   begin:  int
-#           The starting value of the list.
-#   end:    int
-#           The last value
-#
-# Return:
-#   lst:    list of ints
-#           The generated list ranging from begin to end.
-
-
-def generateList(begin, end):
-    lst = []
-    for x in range(begin, end+1):
-        lst.append(x)
-    return lst
-
-
 # getPrime(begin=2, end=1000)
 # This function prints all prime numbers from a list of values in a specified range.
 # Parameters:
@@ -27,25 +7,28 @@ def generateList(begin, end):
 
 def getPrime(max=1000):
     assert max > 2
-    primeLst = generateList(2, max)
-    tmpLst = []
-    index = 0
-    for i in primeLst:
-        for j in range(i+index, len(primeLst), i):
-            tmpLst.append(primeLst[j])
-        index += 1
+    tmp = [True] * (max + 1)
 
-    for k in tmpLst:
-        if k in primeLst:
-            primeLst.pop(primeLst.index(k))
+    for k in range(2, max):
+        waarde = 2 * k
+        while waarde < max:
 
-    print(primeLst)
+            tmp[waarde] = False
+            waarde += k
+
+    primeList = []
+    for i in range(2, max):
+        if tmp[i]:
+            primeList.append(i)
+
+    return primeList
+
 
 # Testing for default value (1000).
-getPrime()
+print(getPrime())
 
 # Testing for small number.
-getPrime(25)
+# getPrime(25)
 
 # Testing for value of 0.
-getPrime(0)
+# getPrime(0)
